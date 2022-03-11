534e23441774c535a237efa037169813f1b8b227
Sydney <sydneyph@udel.edu>
2022-03-10 19:38:55 -0500

    solved MultipleChoiceQuestion

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,9 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
+
+type ChangeEvent = React.ChangeEvent<
+    HTMLTextAreaElement | HTMLInputElement | HTMLSelectElement
+>;
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +12,28 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): JSX.Element {
+    const [userAnswer, setUserAnswer] = useState<string>(options[0]);
+
+    function updateUserAnswer(event: ChangeEvent) {
+        setUserAnswer(event.target.value);
+    }
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+            <div>
+                <Form.Group controlId="userEmotions">
+                    <Form.Label>How do you feel?</Form.Label>
+                    <Form.Select value={userAnswer} onChange={updateUserAnswer}>
+                        {options.map((answer: string) => (
+                            <option key={answer} value={answer}>
+                                {answer}
+                            </option>
+                        ))}
+                    </Form.Select>
+                </Form.Group>
+                <div>You currently have {userAnswer} selected.</div>
+                <div>{expectedAnswer === userAnswer ? "✔️" : "❌"}</div>
+            </div>
         </div>
     );
 }
