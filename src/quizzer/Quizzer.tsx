7bbad04b9da6d7bbc7406ae71985d4107e92a4b5
Sydney <sydneyph@udel.edu>
2022-03-15 12:52:54 -0400

    set up the swap between editor mode and quizzer mode

--- src/quizzer/Quizzer.tsx
+++ src/quizzer/Quizzer.tsx
@@ -1,5 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
 
 export function Quizzer(): JSX.Element {
-    return <h3>Quizzer</h3>;
+    // this bit of the website will determine if the user is in edit mode or quizzer mode
+    const [isEditing, setIsEditing] = useState<boolean>(false);
+
+    function QuizzerMode(): JSX.Element {
+        return <Button>Quizzer Mode</Button>;
+    }
+
+    function EditorMode(): JSX.Element {
+        return <Button>Editing Mode</Button>;
+    }
+    return (
+        <div>
+            <h3>Quizzer</h3>
+            <div>
+                <Form.Check
+                    type="switch"
+                    id="edit-mode-or-quizzer-mode"
+                    label="Swap Modes"
+                    checked={isEditing}
+                    onChange={() => setIsEditing(!isEditing)}
+                />
+            </div>
+            {/* here we are able to swap between the editing interface and the quizzer interface*/}
+            <div>{isEditing ? <EditorMode /> : <QuizzerMode />}</div>
+        </div>
+    );
 }
